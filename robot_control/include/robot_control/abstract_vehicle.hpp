8f9bf8e01c6327abdc76f5a2e8ce4c051d45e982
Blake McHale <mchale.blake@gmail.com>
2022-03-18 19:54:55 -0400

    start cpp node conversion

---
+++ robot_control/include/robot_control/abstract_vehicle.hpp
@@ -0,0 +1,14 @@
+#ifndef ROBOT_CONTROL_ABSTRACT_VEHICLE_HPP_
+#define ROBOT_CONTROL_ABSTRACT_VEHICLE_HPP_
+
+#include <rclcpp>
+
+/**
+ * @brief Abstract class representing common vehicle functionallities and interfaces using ROS.
+ */
+class AbstractVehicle
+{
+    AbstractVehicle();
+}
+
+#endif // ROBOT_CONTROL_ABSTRACT_VEHICLE_HPP_
